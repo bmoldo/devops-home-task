f285fbb5a9142f27914f1e62b1d5cf78666e3505
Bogdan Moldovan <bogdanmoldovan29@gmai.com>
2025-03-24 22:41:36 +0200

    replaced condition with can

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,17 +1,14 @@
 output "function_name" {
-  value       = aws_lambda_function.function[0].function_name
+  value       = can(aws_lambda_function.function[0]) ? aws_lambda_function.function[0].function_name : null
   description = "The name of the Lambda function"
-  condition   = length(aws_lambda_function.function) > 0
 }
 
 output "function_arn" {
-  value       = aws_lambda_function.function[0].arn
+  value       = can(aws_lambda_function.function[0]) ? aws_lambda_function.function[0].arn : null
   description = "The ARN of the Lambda function"
-  condition   = length(aws_lambda_function.function) > 0
 }
 
 output "invoke_arn" {
-  value       = aws_lambda_function.function[0].invoke_arn
+  value       = can(aws_lambda_function.function[0]) ? aws_lambda_function.function[0].invoke_arn : null
   description = "The Invoke ARN of the Lambda function"
-  condition   = length(aws_lambda_function.function) > 0
 }
