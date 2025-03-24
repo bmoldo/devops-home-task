703988bb39635942ef771cd4d22e1959a8ec9b00
Bogdan Moldovan <bogdanmoldovan29@gmai.com>
2025-03-24 22:40:40 +0200

    fixed lambda outputs

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,14 +1,17 @@
 output "function_name" {
+  value       = aws_lambda_function.function[0].function_name
   description = "The name of the Lambda function"
-  value       = aws_lambda_function.function.function_name
+  condition   = length(aws_lambda_function.function) > 0
 }
 
 output "function_arn" {
+  value       = aws_lambda_function.function[0].arn
   description = "The ARN of the Lambda function"
-  value       = aws_lambda_function.function.arn
+  condition   = length(aws_lambda_function.function) > 0
 }
 
 output "invoke_arn" {
-  description = "The invoke ARN of the Lambda function"
-  value       = aws_lambda_function.function.invoke_arn
+  value       = aws_lambda_function.function[0].invoke_arn
+  description = "The Invoke ARN of the Lambda function"
+  condition   = length(aws_lambda_function.function) > 0
 }
