f1b79aebdb0bb1ca5b4312fb3ada380a873988b5
Bogdan Moldovan <bogdanmoldovan29@gmai.com>
2025-03-24 19:42:54 +0200

    fixed s3 variable

--- terraform/modules/s3/variables.tf
+++ terraform/modules/s3/variables.tf
@@ -24,3 +24,8 @@ variable "environment" {
   description = "Deployment environment"
   type        = string
 }
+
+variable "account_id" {
+  description = "AWS account ID"
+  type        = string
+}
