c53f6c49b86f0f6c937df2d2c179488b3df4dc50
Bogdan Moldovan <bogdanmoldovan29@gmai.com>
2025-03-24 23:39:39 +0200

    added networking module

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -29,20 +29,6 @@ module "vpc" {
   environment = var.environment
 }
 
-
-# Allow Lambda to reach RDS
-
-resource "aws_security_group_rule" "allow_lambda_to_rds" {
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  security_group_id        = module.vpc.default_security_group_id
-  source_security_group_id = module.vpc.lambda_security_group_id
-  description              = "Allow Lambda to connect to RDS on port 5432"
-}
-
-
 # RDS Database
 module "rds" {
   source = "../../modules/rds"
@@ -263,4 +249,12 @@ module "api_gateway" {
   environment = var.environment
 
   depends_on = [module.lambda]
+}
+
+
+module "networking" {
+  source = "../../modules/networking"
+
+  rds_sg_id    = module.vpc.default_security_group_id
+  lambda_sg_id = module.vpc.lambda_security_group_id
 }
