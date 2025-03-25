b97e8f8e357354c79663edc562dbd492696003c0
Bogdan Moldovan <bogdanmoldovan29@gmai.com>
2025-03-25 18:59:47 +0200

    updated vpc outputs

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -37,3 +37,11 @@ output "internet_gateway_id" {
   description = "ID of the Internet Gateway"
   value       = aws_internet_gateway.main.id
 }
+
+output "default_security_group_id" {
+  value = aws_vpc.main.default_security_group_id
+}
+
+output "lambda_security_group_id" {
+  value = aws_security_group.lambda.id
+}
