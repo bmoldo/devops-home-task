92c035f193f6b9e0671720b1a63c96b89066e33f
Bogdan Moldovan <bogdanmoldovan29@gmai.com>
2025-03-25 21:21:50 +0200

    fixed dev backend error

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-dev-070503547773"
+    bucket         = "terraform-state-bmoldo-devops-home-task"
     key            = "terraform/dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
