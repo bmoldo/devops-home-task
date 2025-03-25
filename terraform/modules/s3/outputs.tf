96b057c4d426c7fbc72b294ef2a03d4c9a41d19a
Bogdan Moldovan <bogdanmoldovan29@gmai.com>
2025-03-25 22:04:53 +0200

    fixed s3 outputs

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -1,47 +1,6 @@
-# General bucket outputs (update these to use one of your actual resources)
-output "bucket_id" {
-  description = "ID of the S3 bucket"
-  value       = aws_s3_bucket.user_api.id
-}
-
 output "bucket_name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.user_api.bucket
+  value = aws_s3_bucket.user_api.bucket
 }
 
 output "bucket_arn" {
-  description = "ARN of the S3 bucket"
-  value       = aws_s3_bucket.user_api.arn
-}
-
-output "bucket_domain_name" {
-  description = "Domain name of the S3 bucket"
-  value       = aws_s3_bucket.user_api.bucket_domain_name
-}
-
-output "bucket_regional_domain_name" {
-  description = "Regional domain name of the S3 bucket"
-  value       = aws_s3_bucket.user_api.bucket_regional_domain_name
-}
-
-# User API bucket outputs
-output "user_api_bucket_name" {
-  description = "The name of the User API S3 bucket"
-  value       = aws_s3_bucket.user_api.id
-}
-
-output "user_api_bucket_arn" {
-  description = "The ARN of the User API S3 bucket"
-  value       = aws_s3_bucket.user_api.arn
-}
-
-# Lambda zip bucket outputs
-output "lambda_zip_bucket_name" {
-  description = "The name of the Lambda zip S3 bucket"
-  value       = aws_s3_bucket.lambda_zip.id
-}
-
-output "lambda_zip_bucket_arn" {
-  description = "The ARN of the Lambda zip S3 bucket"
-  value       = aws_s3_bucket.lambda_zip.arn
-}
+  value = aws_s3_bucket.user_api.arn
