96f19b334f29a78af5238082e24b237ba5fea4e5
Bogdan Moldovan <bogdanmoldovan29@gmai.com>
2025-03-26 19:03:42 +0200

    fixed development name

--- terraform/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bmoldo-devops-home-task"
-    key            = "terraform/dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-locks-dev"
-  }
-}
