5c055a948fd5e1b49ac50ae5f08795123a7055b2
bmoldo <bogdanmoldovan29@gmail.com>
2025-03-28 14:22:48 +0200

    fixed bucket ref

--- terraform/modules/bootstrap/backend.tf
+++ terraform/modules/bootstrap/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-<your-org>-<your-repo>"
+    bucket = "terraform-state-bmoldo-devops-home-task"
     key    = "bootstrap/terraform.tfstate"
     region = "us-east-1"
   }
