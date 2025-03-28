16a1a3fbda693aa65995660e5647ccc248648c69
bmoldo <bogdanmoldovan29@gmail.com>
2025-03-28 14:16:34 +0200

    added new module

--- terraform/modules/bootstrap/main.tf
+++ terraform/modules/bootstrap/main.tf
@@ -1,7 +1,7 @@
 module "lock_table" {
   source     = "../modules/lock_table"
   table_name = "terraform-locks-${var.environment}"
-  tags       = {
+  tags = {
     Project     = "user-api"
     Environment = var.environment
     ManagedBy   = "Terraform"
