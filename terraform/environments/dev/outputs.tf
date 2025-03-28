ef88b9a94b49e193d286dcc4b729f15c93708acb
Bogdan Moldovan <bogdanmoldovan29@gmai.com>
2025-03-28 18:39:22 +0200

    new output fix

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -50,7 +50,6 @@ output "lambda_packages_bucket" {
 }
 
 output "api_url" {
-  description = "Public API Gateway URL"
-  value       = module.api_gateway[0].api_url
-  condition   = length(module.api_gateway) > 0
+  description = "API Gateway endpoint if available"
+  value       = length(module.api_gateway) > 0 ? module.api_gateway[0].api_url : ""
 }
